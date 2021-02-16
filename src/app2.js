e476c4bd3428e8ce2d472cc7e75f5727372508bd
Davis <326461396@qq.com>
2021-02-16 15:38:01 +0800

    update

--- src/app2.js
+++ src/app2.js
@@ -1,24 +1,74 @@
-import "./app2.css";
-import $ from "jquery";
+import './app2.css'
+import $ from 'jquery'
 
-const $tabBar = $("#app2 .tab-bar");
-const $tabContent = $("#app2 .tab-content");
+const eventBus = $(window)
 
-$tabBar.on("click", "li", e => { // 事件委托
-  const $li = $(e.currentTarget);
-  $li
-    .addClass("selected")
-    .siblings()
-    .removeClass("selected");
-    // 这叫“样式与行为分离”，而不是用jQuery操作css。
-    // 点击一个li，给这个li增加类名selected，同时删除其他li的类名。类名selected的样式在css里设置。
-  const index = $li.index(); // 获取点击的是第几个li
-  $tabContent
-    .children()
-    .eq(index) // 表示选择第几个
-    .addClass("active")
-    .siblings()
-    .removeClass("active");
-});
+const localKey = 'app2.index'
+const m = {
+  data: {
+    index: parseInt(localStorage.getItem(localKey)) || 0
+  },
+  create() {},
+  delete() {},
+  update(data) {
+    Object.assign(m.data, data)
+    eventBus.trigger('m:updated')
+    localStorage.setItem('index', m.data.index)
+  },
+  get() {}
+}
 
-$tabBar.children().eq(0).trigger('click') // 默认点击tab1
+const v = {
+  el: null,
+  html: (index) => {
+    return `
+    <div>
+      <ol class="tab-bar">
+        <li class="${index === 0 ? 'selected' : ''}" data-index="0"><span>1111</span></li>
+        <li class="${index === 1 ? 'selected' : ''}" data-index="1"><span>2222</span></li>
+      </ol>
+      <ol class="tab-content">
+        <li class="${index === 0 ? 'active' : ''}">内容1</li>
+        <li class="${index === 1 ? 'active' : ''}">内容2</li>
+      </ol>
+    </div>
+`
+  },
+  init(container) {
+    v.el = $(container)
+  },
+  render(index) {
+    if (v.el.children.length !== 0) v.el.empty()
+    $(v.html(index)).appendTo(v.el)
+  }
+}
+
+const c = {
+  init(container) {
+    v.init(container)
+    v.render(m.data.index) // view = render(data)
+    c.autoBindEvents()
+    eventBus.on('m:updated', () => {
+      v.render(m.data.index)
+    })
+  },
+  events: {
+    'click .tab-bar li': 'x',
+  },
+  x(e) {
+    const index = parseInt(e.currentTarget.dataset.index)
+    m.update({index: index})
+  },
+  autoBindEvents() {
+    for (let key in c.events) {
+      const value = c[c.events[key]]
+      const spaceIndex = key.indexOf(' ')
+      const part1 = key.slice(0, spaceIndex)
+      const part2 = key.slice(spaceIndex + 1)
+      v.el.on(part1, part2, value)
+    }
+  }
+}
+
+
+export default c
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import "./reset.css";
 import "./global.css";
 
-import "./app1.js";
-import "./app2.js";
+import x from './app1.js' // 引用'./app1.js'导出的默认变量，并命名为x
+import y from './app2.js'
 import "./app3.js";
 import "./app4.js";
+
+x.init('#app1')
+y.init('#app2')
